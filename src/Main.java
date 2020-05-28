2f7f10d31cfab7ed0a8c2c026529ff755606d222
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2020-05-27 22:22:49 -0300

    Finalizado testes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,55 +22,17 @@ public class Main {
 
             switch (opt) {
                 case 1:
-                    definirVertices(grafo);
+                    algoritmoDijsktraExemploProfessor();
                     break;
 
                 case 2:
-                    adicionarAresta(grafo);
-                    break;
-
-                case 3:
-                    adicionarArestaDireto(grafo);
-                    definirVerticeInicial(grafo);
-                    break;
-
-                case 4:
-                    definirVerticeInicial(grafo);
-                    break;
-
-                case 5:
-                    listaArestas(grafo);
-                    break;
-
-                case 6:
-                    algoritmoKruskal(grafo);
-                    break;
-
-                case 7:
-
-                    algoritmoPrimJarnik(grafo);
-                    break;
-
-                case 8:
-                    algoritmoDijsktra(grafo);
-                    break;
-
-                case 9:
-                    algoritmoDijsktraDadosPreCadastrados();
+                    algoritmoDijsktraExemploCriado();
                     break;
 
                 default:
                     break;
             }
         }
-    }
-
-    private static String getConfiguracoes(Grafo grafo) {
-        String retorno = "Grafo: ";
-        retorno += grafo.isOrientado() ? "Orientado" : "Não-Orientado";
-        retorno += ", ";
-        retorno += grafo.isValorado() ? "Valorado" : "Não-Valorado";
-        return retorno + "\t";
     }
 
     private static void configuracoesIniciais(Grafo grafo) {
@@ -96,201 +59,10 @@ public class Main {
 
     public static String getMenu() {
         return "Selecione uma opção" +
-                "\n1 - Definir os Vértices" +
-                "\n2 - Adicionar Uma aresta" +
-                "\n3 - Adicionar várias Arestas" +
-                "\n4 - Definir vértice inicial" +
-                "\n---------------------------------" +
-                "\n5 - Lista de Arestas" +
-                "\n6 - Algoritmo de Kruskal" +
-                "\n7 - Algoritmo de Prim" +
-                "\n8 - Algoritmo de Dijkstra" +
-                "\n9 - Algoritmo de Dijkstra com dados pré-cadastrado" +
+                "\n1 - Executar algoritmo do exemplo do professor!" +
+                "\n2 - Executar algoritmo do exemplo criado por mim!" +
                 "\n---------------------------------" +
                 "\n0 - Sair";
-    }
-
-    public static void definirVertices(Grafo grafo) {
-        String[] nomesVertices = input("Insira os vértices do grafo separados por vírgula. \nExemplo: A, B, C, D")
-                .toUpperCase()
-                .replace(" ", "")
-                .split(",");
-
-        grafo.getVertices().clear();
-        grafo.getArestas().clear();
-
-        for (int i = 0; i < nomesVertices.length; i++) {
-            Integer idVertice = i + 1;
-            grafo.getVertices().add(new Vertice(idVertice, nomesVertices[i], NAO_VISITADO));
-        }
-    }
-
-    public static void definirVerticeInicial(Grafo grafo) {
-        String resultado = "\n";
-        for (Vertice vertice : grafo.getVertices()) {
-            resultado += "\n" + vertice.getIdVertice() + " - " + vertice.getNome();
-        }
-        Vertice vertice = obterVertice(grafo, Integer.parseInt(input("Selecione o código do vértice inicial: \n" + resultado)));
-        vertice.setInicial(true);
-        vertice.setValor(0);
-    }
-
-    public static void adicionarArestaDireto(Grafo grafo) {
-        String mensagem = "Insira as arestas pelo nome do vértice, utilizando vírgula entre origem e destino, \nalém de ponto e vírgula para separar os vértices, Conforme exemplo:\n";
-        String exemplo = "(ORIGEM,DESTINO;ORIGEM,DESTINO) \n\n0,1;0,3;1,4\n\n";
-        String verticesCadastrados = "";
-
-        for (Vertice vertice : grafo.getVertices()) {
-            verticesCadastrados += vertice.getNome() + "\n";
-        }
-        String input = input(mensagem + exemplo + verticesCadastrados);
-        String[] arrayVertices = input.replace(" ", "")
-                .replace("(", "")
-                .replace(")", "")
-                .split(";");
-
-        for (String vertice : arrayVertices) {
-            String[] verticesOrigemDestino = vertice.split(",");
-            Vertice origem = obterVertice(grafo, verticesOrigemDestino[0]);
-            Vertice destino = obterVertice(grafo, verticesOrigemDestino[1]);
-
-            int valorAresta = 0;
-
-            if (grafo.isValorado())
-                valorAresta = Integer.parseInt(input("Insira valor para a Aresta: " + origem.getNome() + " - " + destino.getNome())
-                        .toUpperCase()
-                        .replace(" ", ""));
-
-            Aresta aresta = new Aresta(origem, destino, valorAresta, "e" + (grafo.getArestas().size() + 1));
-            grafo.getArestas().add(aresta);
-        }
-    }
-
-    public static void adicionarAresta(Grafo grafo) {
-        String verticeOrigem = "Escolha pelo nome, o vértice de ORIGEM!\n";
-        String verticeDestino = "Agora escolha pelo nome, o vértice de DESTINO!\n";
-        String verticesCadastrados = "";
-
-        for (Vertice vertice : grafo.getVertices()) {
-            verticesCadastrados += vertice.getIdVertice() + " - " + vertice.getNome() + "\n";
-        }
-        Vertice origem = null;
-        Vertice destino = null;
-
-        while (origem == null && destino == null) {
-            origem = obterVertice(grafo, input(verticeOrigem + verticesCadastrados));
-            destino = obterVertice(grafo, input(verticeDestino + verticesCadastrados));
-        }
-
-        int valorAresta = 0;
-
-        if (grafo.isValorado())
-            valorAresta = Integer.parseInt(input("Insira valor para a Aresta.").toUpperCase().replace(" ", ""));
-
-        Aresta aresta = new Aresta(origem, destino, valorAresta, "e" + (grafo.getArestas().size() + 1));
-        grafo.getArestas().add(aresta);
-
-        if (!grafo.isOrientado()) {
-            Aresta arestaNaoOrientada = new Aresta(destino, origem, valorAresta, "e" + (grafo.getArestas().size() + 1));
-            grafo.getArestas().add(arestaNaoOrientada);
-        }
-    }
-
-    public static Vertice obterVertice(Grafo grafo, String nomeVertice) {
-        for (Vertice vertice : grafo.getVertices()) {
-            if (vertice.getNome().equals(nomeVertice))
-                return vertice;
-        }
-        output("Vértice não encontrado.", "Erro");
-        return null;
-    }
-
-    public static Vertice obterVertice(Grafo grafo, Integer idVertice) {
-        for (Vertice vertice : grafo.getVertices()) {
-            if (vertice.getIdVertice().equals(idVertice))
-                return vertice;
-        }
-        output("Vértice não encontrado.", "Erro");
-        return null;
-    }
-
-    private static void listaArestas(Grafo grafo) {
-        String listaArestas = getConfiguracoes(grafo) + " \n\n";
-
-        for (Aresta aresta : grafo.getArestas()) {
-            listaArestas += "[" + aresta.getOrigem() + ", " + aresta.getDestino();
-            listaArestas += grafo.isValorado() ? ", " + aresta.getValor() + "]\n" : "]\n";
-        }
-
-        output(listaArestas, "Lista de Arestas");
-    }
-
-    private static void algoritmoKruskal(Grafo grafo) {
-        List<Aresta> arestas = grafo.getArestas();
-        List<Vertice> vertices = grafo.getVertices();
-        arestas.sort(Comparator.comparing(Aresta::getValor));
-
-        List<Vertice[]> filaVerticesOrdemCrescente = new ArrayList<>();
-        for (Vertice vertice : vertices) {
-            Vertice[] arrayVertice = new Vertice[1];
-            arrayVertice[0] = vertice;
-            filaVerticesOrdemCrescente.add(arrayVertice);
-        }
-
-        StringBuilder resultado = new StringBuilder("Tabela\n\n");
-
-        int vMenosUm = 0;
-        for (Aresta aresta : arestas) {
-            manipularAresta(aresta, filaVerticesOrdemCrescente, resultado, vMenosUm);
-            if (vMenosUm == vertices.size() - 1)
-                break;
-        }
-
-        output(resultado.toString(), "Algoritmo de Kruskal");
-    }
-
-    private static void manipularAresta(Aresta aresta, List<Vertice[]> filaVerticesOrdemCrescente, StringBuilder resultado, int vMenosum) {
-        Vertice origem = aresta.getOrigem();
-        Vertice destino = aresta.getDestino();
-
-        Vertice[] arrayVerticeOrigem = obterArrayOndeEstaOVertice(origem, filaVerticesOrdemCrescente);
-        Vertice[] arrayVerticeDestino = obterArrayOndeEstaOVertice(destino, filaVerticesOrdemCrescente);
-
-        if (!arrayVerticeOrigem.equals(arrayVerticeDestino)) {
-            Vertice[] uniaoArrayVertices = new Vertice[(arrayVerticeOrigem.length + arrayVerticeDestino.length)];
-
-            int i = 0;
-
-            for (int j = 0; j < arrayVerticeOrigem.length; j++) {
-                uniaoArrayVertices[i] = arrayVerticeOrigem[j];
-                i++;
-            }
-
-            for (int j = 0; j < arrayVerticeDestino.length; j++) {
-                uniaoArrayVertices[i] = arrayVerticeDestino[j];
-                i++;
-            }
-
-            filaVerticesOrdemCrescente.remove(arrayVerticeOrigem);
-            filaVerticesOrdemCrescente.remove(arrayVerticeDestino);
-            filaVerticesOrdemCrescente.add(uniaoArrayVertices);
-            resultado.append("Valor: " + aresta.getValor());
-            resultado.append("\tOrigem: " + origem.getNome());
-            resultado.append("\tDestino: " + destino.getNome());
-            resultado.append("\n");
-            vMenosum++;
-        }
-
-    }
-
-    private static Vertice[] obterArrayOndeEstaOVertice(Vertice vertice, List<Vertice[]> setVertices) {
-        for (Vertice[] arrayVertice : setVertices) {
-            for (int i = 0; i < arrayVertice.length; i++) {
-                if (arrayVertice[i].equals(vertice))
-                    return arrayVertice;
-            }
-        }
-        return null;
     }
 
     private static List<Vertice> obterAdjacentes(boolean orientado, List<Aresta> arestas, Vertice vertice) {
@@ -334,56 +106,11 @@ public class Main {
         return 0;
     }
 
-    private static void algoritmoPrimJarnik(Grafo grafo) throws CloneNotSupportedException {
-        List<Aresta> arestas = grafo.getArestas();
-        List<Vertice> vertices = grafo.getVertices();
-        List<Vertice> verticeQueue = new ArrayList<>(vertices);
-        List<Vertice> verticesVisitados = new ArrayList<>();
-        Vertice verticeVisitado = verticeMenorValor(verticeQueue);
-
-        while (!verticeQueue.isEmpty()) {
-
-            Vertice verticeMenorValor = new Vertice(Integer.MAX_VALUE);
-            for (Vertice adjacente : obterAdjacentes(false, arestas, verticeVisitado)) {
-                if (adjacente.getCor().equals(NAO_VISITADO)) {
-                    int valorVertice = valorArestaEntreVertices(verticeVisitado, adjacente, arestas);
-                    adjacente.setValor(valorVertice);
-
-                    if (adjacente.getValor() < verticeMenorValor.getValor())
-                        verticeMenorValor = adjacente;
-                }
-            }
-
-            verticeMenorValor.setCaminho(verticeVisitado.clone());
-            verticesVisitados.add(verticeVisitado.clone());
-            verticeQueue.remove(verticeVisitado.clone());
-            verticeVisitado = verticeMenorValor;
-            verticeVisitado.setCor(VISITADO);
-        }
-
-        StringBuilder resultado = new StringBuilder("Tabela\n\n");
-        for (Vertice vertice : verticesVisitados) {
-            resultado.append("Vértice: " + vertice.getNome());
-            resultado.append(" - Valor: ");
-            resultado.append(vertice.getValor());
-            resultado.append("\n");
-        }
-        output(resultado.toString(), "Teste");
-    }
-
-    private static Vertice verticeMenorValor(List<Vertice> vertices) {
-        Vertice verticeMenorValor = new Vertice(Integer.MAX_VALUE);
-        for (Vertice vertice : vertices) {
-            if (vertice.getValor() < verticeMenorValor.getValor())
-                verticeMenorValor = vertice;
-        }
-        return verticeMenorValor;
-    }
-
-    private static void algoritmoDijsktraDadosPreCadastrados() throws CloneNotSupportedException {
+    private static void algoritmoDijsktraExemploProfessor() throws CloneNotSupportedException {
         Grafo grafo = new Grafo();
         grafo.getVertices().clear();
         grafo.getArestas().clear();
+
 
         Vertice vertice1 = new Vertice(1, "1", NAO_VISITADO);
         Vertice vertice2 = new Vertice(2, "2", NAO_VISITADO);
@@ -402,7 +129,7 @@ public class Main {
         Vertice vertice15 = new Vertice(15, "15", NAO_VISITADO);
 
         grafo.setVerticeInicial(vertice1);
-        grafo.setVerticeFinal(vertice13);
+        grafo.setVerticeFinal(vertice14);
 
         grafo.getVertices().add(vertice1);
         grafo.getVertices().add(vertice2);
@@ -459,6 +186,55 @@ public class Main {
         grafo.getArestas().add(aresta17);
         grafo.getArestas().add(aresta18);
 
+
+        algoritmoDijsktra(grafo);
+    }
+
+    private static void algoritmoDijsktraExemploCriado() throws CloneNotSupportedException {
+        Grafo grafo = new Grafo();
+        grafo.getVertices().clear();
+        grafo.getArestas().clear();
+
+        Vertice vertice1 = new Vertice(1, "1", NAO_VISITADO);
+        Vertice vertice2 = new Vertice(2, "2", NAO_VISITADO);
+        Vertice vertice3 = new Vertice(3, "3", NAO_VISITADO);
+        Vertice vertice4 = new Vertice(4, "4", NAO_VISITADO);
+        Vertice vertice5 = new Vertice(5, "5", NAO_VISITADO);
+        Vertice vertice6 = new Vertice(6, "6", NAO_VISITADO);
+        Vertice vertice7 = new Vertice(7, "7", NAO_VISITADO);
+
+        grafo.setVerticeInicial(vertice1);
+        grafo.setVerticeFinal(vertice7);
+
+        grafo.getVertices().add(vertice1);
+        grafo.getVertices().add(vertice2);
+        grafo.getVertices().add(vertice3);
+        grafo.getVertices().add(vertice4);
+        grafo.getVertices().add(vertice5);
+        grafo.getVertices().add(vertice6);
+        grafo.getVertices().add(vertice7);
+
+        //Aresta aresta = new Aresta(origem, destino, valorAresta, "e" + (grafo.getArestas().size() + 1));
+        Aresta aresta1 = new Aresta(vertice1, vertice2, 6, "e1");
+        Aresta aresta2 = new Aresta(vertice1, vertice4, 2, "e1");
+        Aresta aresta3 = new Aresta(vertice2, vertice5, 3, "e1");
+        Aresta aresta4 = new Aresta(vertice3, vertice7, 1, "e1");
+        Aresta aresta5 = new Aresta(vertice4, vertice5, 3, "e1");
+        Aresta aresta6 = new Aresta(vertice4, vertice6, 5, "e1");
+        Aresta aresta7 = new Aresta(vertice5, vertice3, 2, "e1");
+        Aresta aresta8 = new Aresta(vertice5, vertice7, 4, "e1");
+        Aresta aresta9 = new Aresta(vertice6, vertice5, 4, "e1");
+
+        grafo.getArestas().add(aresta1);
+        grafo.getArestas().add(aresta2);
+        grafo.getArestas().add(aresta3);
+        grafo.getArestas().add(aresta4);
+        grafo.getArestas().add(aresta5);
+        grafo.getArestas().add(aresta6);
+        grafo.getArestas().add(aresta7);
+        grafo.getArestas().add(aresta8);
+        grafo.getArestas().add(aresta9);
+
         algoritmoDijsktra(grafo);
     }
 
@@ -477,7 +253,7 @@ public class Main {
         while (!verticesNaoVisitados.isEmpty()) {
             Vertice origem = menorValorNaoVisitado(verticesNaoVisitados);
 
-            if(origem == null)
+            if (origem == null)
                 return;
 
             Vertice verticeMenorValor = new Vertice(Integer.MAX_VALUE);
@@ -485,7 +261,7 @@ public class Main {
                 int valorAresta = valorArestaEntreVertices(origem, destino, arestas);
                 int custo = origem.getValor() + valorAresta;
 
-                if(custo < 0)
+                if (custo < 0)
                     break;
 
                 if (custo < verticeMenorValor.getValor() && custo < destino.getValor()) {
@@ -505,15 +281,24 @@ public class Main {
 
     private static void mostrarCaminho(Vertice verticeFinal) {
         Vertice verticeAtual = verticeFinal;
-        while (verticeAtual != null) {
-            System.out.println(verticeAtual.getNome() + " - " + verticeAtual.getValor());
 
+        List<String> caminho = new ArrayList<>();
+
+        StringBuilder resultado = new StringBuilder("Resultado\n\n");
+        while (verticeAtual != null) {
+            caminho.add(verticeAtual.getNome());
             verticeAtual = verticeAtual.getCaminho();
         }
+
+        Collections.reverse(caminho);
+
+        caminho.forEach(r -> resultado.append(r + " -> "));
+
+        output(resultado.toString(), "Resultado");
     }
 
     private static Vertice menorValorNaoVisitado(List<Vertice> verticesNaoVisitados) {
-        if(verticesNaoVisitados.size() > 0) {
+        if (verticesNaoVisitados.size() > 0) {
             verticesNaoVisitados.sort(Comparator.comparing(Vertice::getValor));
             return verticesNaoVisitados.get(0);
         }
